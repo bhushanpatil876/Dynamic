db088b1cf924bcf57140910584602b8a91ab2ce7
bhushan-patil <bhushanpatil76201@gmail.com>
2026-06-02 16:42:33 +0530

    LLM\dynamic.py

---
+++ LLM/dynamic.py
@@ -0,0 +1,61 @@
+import langchain_core
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+import os
+import streamlit as st
+from langchain_core.prompts import PromptTemplate
+
+
+load_dotenv()
+
+model = ChatGroq(
+    model="llama-3.1-8b-instant",
+    api_key=os.getenv("GROQ_API_KEY")
+)
+
+st.header('Reasearch Tool')
+
+paper_input = st.selectbox( "Select Reasearch Paper Name", ["Attention Is All You Need",
+ "BERT: Pre-training of Deep Bidirecional Transformers", "GPT-3: Language MOdels are "
+ "Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"])
+
+style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", 
+"Code-Oriented", "Mathematical"] )
+
+length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium "
+"(3-5 paragraphs)", "Long (detailed explanation)"])
+
+from langchain_core.prompts import PromptTemplate
+
+template = PromptTemplate(
+    template="""
+Please summarize the research paper titled "{paper_input}" with the following specifications:
+Explanation Style: {style_input}
+Explanation Length: {length_input}
+
+1. Mathematical Details:
+   - Include relevant mathematical equations if present in the paper.
+   - Explain the mathematical concepts using simple, intuitive code snippets where applicable.
+
+2. Analogies:
+   - Use relatable analogies to simplify complex ideas.
+If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.
+
+Ensure the summary is clear, accurate, and aligned with the provided style and length.
+""",
+    input_variables=["paper_input", "style_input", "length_input"]
+)
+
+
+
+if st.button("Summarize"):
+    chain = template | model
+    result = chain.invoke({
+         "paper_input": paper_input,
+          "style_input": style_input,
+         "length_input": length_input
+   })
+    st.write(result.content)
+
+# python LLM\dynamic.py
+# streamlit run LLM\dynamic.py
